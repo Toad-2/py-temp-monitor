739288a7fc173f1729f45200ea5850c9a38039fb
toad2134 <104324796+toad2134@users.noreply.github.com>
2022-04-30 17:42:45 -0700

    refactoring and added documentation
    - added comment documentation throughout code
    - refactored code to allow for external import by graph.py

--- runtime.py
+++ runtime.py
@@ -1,7 +1,3 @@
-# imports
-from glob import glob
-from time import time, localtime, sleep
-
 # time between data pull in minutes
 checkTime = 5
 
@@ -10,40 +6,30 @@ checkTime = 5
 # +1 accounts for header
 keep = ((60/checkTime) * 24) + 1
 
-# finds and prepares one wire devices devices
-base_path = '/sys/bus/w1/devices/'
-dev_search = glob(base_path + '28*')
-probe1 = dev_search[0] + '/w1_slave'
-probe2 = dev_search[1] + '/w1_slave'
-
-print("Temp probes initialized")
-
-# creates JSON log file
-timestamp = localtime()
-filename = f'temp_log-{timestamp[1]}_{timestamp[2]}_{timestamp[0]}_{timestamp[3]}_{timestamp[4]}.csv'
-
-with open(filename, 'w') as file:
-    file.write(f"{int(round(time(),0))},probe 1,probe 2\n")
-print("log file made")
-
 # reads most recent data from sensor, converts to C and returns
 def reader(given):
+    # opens probe read vector and dumps contents to array
     with open(given, 'r') as f:
         lines = f.readlines()
+
+    # strips extra data from array and pulls data as millicelsius
     workingVal = float(lines[1][-6:])
+
+    # converts millicelsius to celsius
     tempC = workingVal/1000
 
     # returns temperature data for the probe in celsius rounded to the 100th
     return round(tempC, 2)
 
+# takes values from probes and appends to rolling csv log
 def file_append(e1, e2):
     # opens log file and dumps all entries to list
     with open(filename, 'r') as f:
         given = f.readlines()
 
     # Takes length of list
-    # If over specified length, removes second item, appends new item and returns list to csv file.
     # If not over length, appends entry to bottom of csv file.
+    # If over specified length, removes second item, appends new item and returns list to csv file.
     if len(given) <= keep - 1:  # -1 accounts for list indexing from zero
         with open(filename, 'a') as l:
             l.write("{},{},{}".format(int(round(time(),0)), e1, e2) + '\n')
@@ -54,13 +40,39 @@ def file_append(e1, e2):
             for i in given:
                 e.write(i)
 
-# checkTime minutes to seconds
-checkSec = checkTime * 60
+if __name__ == '__main__':
+    # imports
+    # imports are inside if statement to hopefully prevent external routine graph.py from wasting time on importing them when it calls for var keep
+    from glob import glob
+    from time import time, localtime, sleep
 
-while True:
-    one = reader(probe1)
-    two = reader(probe2)
-    print(one, two)
+    # finds and prepares one wire devices devices
+    base_path = '/sys/bus/w1/devices/'
+    dev_search = glob(base_path + '28*')
 
-    file_append(one, two)
-    sleep(checkSec)
+    # assigns found probes to variables and appends with read vector
+    probe1 = dev_search[0] + '/w1_slave'
+    probe2 = dev_search[1] + '/w1_slave'
+
+    print("Temp probes initialized")
+
+    # creates JSON log file
+    timestamp = localtime()
+    filename = f'temp_log-{timestamp[1]}_{timestamp[2]}_{timestamp[0]}_{timestamp[3]}_{timestamp[4]}.csv'
+
+    with open(filename, 'w') as file:
+        file.write(f"{int(round(time(), 0))},probe 1,probe 2\n")
+
+    print("log file made")
+
+    # checkTime minutes to seconds
+    checkSec = checkTime * 60
+    while True:
+        # reads data from probes using reader function and assigns returned values to variables
+        one = reader(probe1)
+        two = reader(probe2)
+
+        # appends temp entries to file using file_append function
+        file_append(one, two)
+
+        sleep(checkSec)
